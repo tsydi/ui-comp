a3dbf1e6c7141ccd05533397910a76dda09fa782
TsydykhP <tsydykhp@gmail.com>
2021-09-27 00:01:49 +0800

    created Layout with <Head> ta

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+
+interface IHead {
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  keywords: string;
+}
+
+export const Layout = ({ children, title, description, keywords } : IHead) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="robots" content="index,follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <title>{title}</title>
+      </Head>
+      {children}
+    </>
+  )
+}
